7f4601c11b9ceb716e36ac53dbaac0fa1785449b
manojs-sudo <manojs@msqube.com>
2026-05-06 02:16:50 +0530

    pushed

--- src/enviroments.ts
+++ src/enviroments.ts
@@ -1,5 +1,5 @@
 // src/constants/config.ts
-export const API_BASE_URL = 'http://localhost:5000/api';
+export const API_BASE_URL = 'https://manoj.ycsas.in/api';
 
 // export const OFFICE_LAT = 22.77930;
 // export const OFFICE_LNG =  88.25524;
